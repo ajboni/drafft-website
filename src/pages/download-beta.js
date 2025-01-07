f8a7e8a373fb7c63e20d5ca8ce3ab164a8334658
Alexis Boni <mail@aboni.dev>
2025-01-06 21:26:00 -0300

    wip

--- src/pages/download-beta.js
+++ src/pages/download-beta.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import Head from "@docusaurus/Head";
 
 export default function MyReactPage() {
   return (
     <Layout title={`Try Drafft Beta.`} description="Try the beta version of Drafft.">
+      <Head>
+        <script async src="https://tally.so/widgets/embed.js"></script>
+      </Head>
       <main className="w-full mx-auto max-w-7xl px-6 lg:px-8">
-      <iframe data-tally-src="https://tally.so/embed/mRv5z4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="1364" frameborder="0" marginheight="0" marginwidth="0" title="Beta will be available soon."></iframe>
+        <div className="max-w-2xl mx-auto">
+          <iframe
+            data-tally-src="https://tally.so/embed/mRv5z4?hideTitle=1&transparentBackground=1&dynamicHeight=1"
+            loading="lazy"
+            width="100%"
+            height="689"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            title="Drafft 2 Beta will be available soon."
+          ></iframe>
+        </div>
       </main>
     </Layout>
   );
